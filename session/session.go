d3d2638509db52f77fba8e1312a4ad1e5a60abee
TuringChen <TuringChen@upchina.com>
2018-05-08 18:56:21 +0800

    updata session manager

--- session/session.go
+++ session/session.go
@@ -4,7 +4,11 @@ import (
 	"crypto/rand"
 	"encoding/base64"
 	"fmt"
+	"net/http"
+	"net/url"
 	"sync"
+	"text/template"
+	"time"
 )
 
 // 全局的 session 管理器
@@ -15,8 +19,59 @@ type Manager struct {
 	maxLifeTime int64
 }
 
+// Manager Method: 创建全局唯一的 sessionID
+func (manager *Manager) sessionID() string {
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		return ""
+	}
+	return base64.URLEncoding.EncodeToString(b)
+}
+
+// Manager Method: 用来检测是否已经有某个Session与当前来访用户发生了关联，如果没有则创建之
+func (manager *Manager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
+	manager.lock.Lock()         // 使用互斥锁保证互斥执行
+	defer manager.lock.Unlock() // 解锁
+	cookie, err := r.Cookie(manager.cookieName)
+	// 没找到来自客户端的 sessionID
+	if err != nil || cookie.Value == "" {
+		sid := manager.sessionID()
+		// 服务器创建一个 session
+		session, _ = manager.provider.SessionInit(sid)
+		cookie := http.Cookie{Name: manager.cookieName, Value: url.QueryEscape(sid), Path: "/", HttpOnly: true, MaxAge: int(manager.maxLifeTime)}
+		http.SetCookie(w, &cookie)
+	} else {
+		sid, _ := url.QueryUnescape(cookie.Value)
+		session, _ = manager.provider.SessionRead(sid)
+	}
+	return
+}
+
+// Manager Method: Destroy sessionid
+func (manager *Manager) SessionDestroy(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie(manager.cookieName)
+	if err != nil || cookie.Value == "" {
+		return
+	} else {
+		manager.lock.Lock()
+		defer manager.lock.Unlock()
+		manager.provider.SessionDestroy(cookie.Value)
+		expiration := time.Now()
+		cookie := http.Cookie{Name: manager.cookieName, Path: "/", HttpOnly: true, Expires: expiration, MaxAge: -1}
+		http.SetCookie(w, &cookie)
+	}
+}
+
+// Manager Method: session 销毁
+func (manager *Manager) GC() {
+	manager.lock.Lock()
+	defer manager.lock.Unlock()
+	manager.provider.SessionGC(manager.maxLifeTime)
+	time.AfterFunc(time.Duration(manager.maxLifeTime), func() { manager.GC() })
+}
+
 func NewManager(provideName, cookieName string, maxLifeTime int64) (*Manager, error) {
-	provider, ok := provides[ProvideName]
+	provider, ok := provides[provideName]
 	if !ok {
 		return nil, fmt.Errorf("session: unknown provide %q (forgotten import?", provideName)
 	}
@@ -59,18 +114,23 @@ func Register(name string, provider Provider) {
 	provides[name] = provider
 }
 
-// 全局唯一的 sessionID
-func (manager *Manager) sessionId() string {
-	b := make([]byte, 32)
-	if _, err := rand.Read(b); err != nil {
-		return ""
-	}
-	return base64.URLEncoding.EncodeToString(b)
-}
-
 var globalSessions *session.Manager
 
 func init() {
 	// 初始化一个全局的session 管理器
 	globalSessions, _ = NewManager("memory", "gosessionid", 3600)
+}
+
+// 使用 session
+func login(w http.ResponseWriter, r *http.Request) {
+	sess := globalSessions.SessionStart(w, r)
+	r.ParseForm()
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles()
+		w.Header().Set("Content-Type", "text/html")
+		t.Execute(w, sess.Get("username"))
+	} else {
+		sess.Set("username", r.Form["username"])
+		http.Redirect(w, r, "/", 302)
+	}
 }
