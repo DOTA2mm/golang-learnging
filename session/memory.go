9e0061c2666c672c03b84e5de759c2ba001868f2
Chuck <tengchenc@gmail.com>
2018-05-15 00:05:22 +0800

    update session store

--- session/memory.go
+++ session/memory.go
@@ -1,6 +1,12 @@
 package memory
 
+/**
+* 基于内存的 session 存储接口的实现
+ */
+
 import (
+	"container/list"
+	"sync"
 	"time"
 )
 
@@ -16,4 +22,83 @@ func (st *SessionStore) Set(key, value interface{}) error {
 	st.value[key] = value
 	pder.SessionUpdate(st.sid)
 	return nil
+}
+
+func (st *SessionStore) Get(key interface{}) interface{} {
+	pder.SessionUpdate(st.sid)
+	if v, ok := st.value[key]; ok {
+		return v
+	} else {
+		return nil
+	}
+}
+
+func (st *SessionStore) Delete(key interface{}) error {
+	delete(st.value, key)
+	pder.SessionUpdate(st.sid)
+	return nil
+}
+
+func (st *SessionStore) SessionID() string {
+	return st.sid
+}
+
+type Provider struct {
+	lock     sync.Mutex               // 用来锁
+	sessions map[string]*list.Element // 用来存储在内存
+	list     *list.List               // 用来做gc
+}
+
+func (pder *Provider) SessionInit(sid string) (session.Session, error) {
+	pder.lock.Lock()
+	defer pder.lock.Unlock()
+	v := make(map[interface{}]interface{}, 0)
+	newsess := &SessionStore{sid: sid, timeAccessed: time.Now(), value: v}
+	element := pder.list.PushBack(newsess)
+	return newsess, nil
+}
+
+func (pder *Provider) SessionRead(sid string) (session.Session, error) {
+	if element, ok := pder.sessions[sid]; ok {
+		return element.Value.(*SessionStore), nil
+	} else {
+		sess, err := pder.SessionInit(sid)
+		return sess, nil
+	}
+	return nil, nil
+}
+
+func (pder *Provider) SessionGC(maxlifetime int64) {
+	pder.lock.Lock()
+	defer pder.lock.Unlock()
+
+	for {
+		element := pder.list.Back()
+		if element == nil {
+			break
+		}
+		// 删除已过期的 session
+		if (element.Value.(*SessionStore).timeAccessed.Unix() + maxlifetime) < time.Now().Unix() {
+			pder.list.Remove(element)
+			delete(pder.sessions, element.Value.(*SessionStore).sid)
+		} else {
+			break
+		}
+	}
+}
+
+func (pder *Provider) SessionUpdate(sid string) error {
+	pder.lock.Lock()
+	defer pder.lock.Unlock()
+	if element, ok := pder.sessions[sid]; ok {
+		element.Value.(*SessionStore).timeAccessed = time.Now()
+		pder.list.MoveToFront(element)
+		return nil
+	}
+	return nil
+}
+
+func init() {
+	pder.sessions = make(map[string]*list.Element, 0)
+	session.Register("memory", pder)
 }
